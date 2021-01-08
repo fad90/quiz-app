50a7740654150fd4b27df8c809d3062c27d57200
Dima <fadeev2990@gmail.com>
2021-01-08 21:59:57 +0400

    added questions

---
+++ main.js
@@ -0,0 +1,43 @@
+const quizData = [
+    {
+        question: "How many continents are there in the world?",
+        a: "5",
+        b: "6",
+        c: "7",
+        d: "8",
+        correct: "c",
+    },
+    {
+        question: "What's the Biggest Animal in the World?",
+        a: "Blue Whale",
+        b: "Colossal Squid",
+        c: "African Elephant",
+        d: "Whale Shark",
+        correct: "a",
+    },
+    {
+        question: "How many planets are there in the solar system?",
+        a: "7",
+        b: "8",
+        c: "9",
+        d: "10",
+        correct: "b",
+    },
+
+    {
+        question: "What's the largest country in the world?",
+        a: "USA",
+        b: "Russia",
+        c: "Canada",
+        d: "China",
+        correct: "b",
+    },
+    {
+        question: "What are the most spoken languages in the world?",
+        a: "Hindi",
+        b: "English",
+        c: "Spanish",
+        d: "Chinese",
+        correct: "d",
+    },
+]; 
